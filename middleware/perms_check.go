ebe83a3f30bb0e19e20c4016b4e137f063244476
lanwenhong <hexiexuanlv@126.com>
2023-12-21 14:17:12 +0800

    fixbug

--- middleware/perms_check.go
+++ middleware/perms_check.go
@@ -2,7 +2,9 @@ package middleware
 
 import (
 	"context"
+	"usercenter/respcode"
 	"usercenter/session"
+	ut "usercenter/util"
 
 	"github.com/gin-gonic/gin"
 	"github.com/lanwenhong/lgobase/logger"
@@ -18,8 +20,26 @@ func CheckPerms(perms []string) gin.HandlerFunc {
 		logger.Debugf(ctx, "sk: %s", sk)
 		se := session.NewSession(sk.(string))
 		value, _ := se.GetData(ctx, "allperm")
-		allperm := value.([]interface{})
-		logger.Debugf(ctx, "allperm: %v", allperm)
-		c.Next()
+		in_allperm := value.([]interface{})
+		logger.Debugf(ctx, "allperm: %v len: %d", in_allperm, len(in_allperm))
+		in_isadmin, _ := se.GetData(ctx, "isadmin")
+		isadmin := int64(in_isadmin.(float64))
+		logger.Debugf(ctx, "isadmin: %d", isadmin)
+		allperm := []string{}
+		for _, v := range in_allperm {
+			allperm = append(allperm, v.(string))
+		}
+		user_perm_set := ut.NewSet[string]()
+		user_perm_set.SetList(ctx, allperm)
+		check_perm_set := ut.NewSet[string]()
+		check_perm_set.SetList(ctx, perms)
+
+		if is_sub := user_perm_set.IsSubSet(ctx, check_perm_set); !is_sub || isadmin != 1 {
+			respcode.RetError[string](c, respcode.ERR, "permission deny", "", "")
+			c.Abort()
+		} else {
+
+			c.Next()
+		}
 	}
 }
